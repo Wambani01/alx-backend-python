ccb7f89d9da53103b9878ceb885b89ac192cf769
Wambani01 <108225824+Wambani01@users.noreply.github.com>
2023-07-10 14:00:16 +0300

    python async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,11 +6,9 @@ from heapq import nsmallest
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """concurrent execution"""
     delays = []
     for i in range(n):
         delays.append(await wait_random(max_delay))
-    return nsmallest(n, delays)   
-   
+    return nsmallest(n, delays)
