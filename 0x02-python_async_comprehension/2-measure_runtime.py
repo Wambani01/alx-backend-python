ff58d7ac2196bac8fadf3a7b7cb48b61e249d31b
Wambani01 <108225824+Wambani01@users.noreply.github.com>
2023-07-11 11:48:52 +0300

    Async Comprehension

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,6 +7,11 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime():
     start_time = time.time()
-    await asyncio.gather(async_comprehension(), async_comprehension(),async_comprehension(),async_comprehension())
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+        )
     end_time = time.time() - start_time
     return end_time
